d9d5d56586af8fcb19410e9d7f8627f5f78cc382
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2019-11-19 12:27:55 +0900

    Add files via upload

---
+++ apple2orange.sh
@@ -0,0 +1,7 @@
+﻿
+echo "download from \"Index of /~taesung_park/CycleGAN/datasets\""
+URL=https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/apple2orange.zip
+echo $URL
+wget -N $URL
+unzip apple2orange.zip
+rm apple2orange.zip
---
+++ lfw.sh
@@ -0,0 +1,7 @@
+﻿
+echo "download from \"Labeled Faces in the Wild\""
+URL=http://vis-www.cs.umass.edu/lfw/lfw.tgz
+echo $URL
+wget -N $URL
+tar zxf lfw.tgz
+rm lfw.tgz
